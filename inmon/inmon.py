8b81fe2e6cad647546fcfda56092e3ce4a39ca2d
João Lopes <linux-man@hotmail.com>
2021-12-27 03:29:28 +0000

    Add Copyright

--- inmon/inmon.py
+++ inmon/inmon.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# Copyright 2021 Caldas Lopes
+# Inmon is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+
 app_name = "Inmon"
 version = "0.3"
 
